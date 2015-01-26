4cd983f1b2a7cd80a46455a73a755deb386ea2ac
labby <office@cms-lab.com>
2015-01-26 15:28:25 +0100

    update lib to release 1.8.0

--- info.php
+++ info.php
@@ -34,7 +34,7 @@ if (defined('LEPTON_PATH')) {
 $module_directory = 'lib_semantic';
 $module_name      = 'Semantic Library';
 $module_function  = 'library';
-$module_version   = '1.7.3.0';
+$module_version   = '1.8.0.0';
 $module_platform  = '2.x';
 $module_delete	  =  true;
 $module_author    = 'cms-lab';
